c7971601f4177e6077d8de22705f30174445d869
Цой Влад <v.tsoi@dnomads.pro>
2024-08-17 13:38:58 +0700

    WIP: create version catalog

--- client-android/app/build.gradle.kts
+++ client-android/app/build.gradle.kts
@@ -47,29 +47,6 @@ android {
 dependencies {
     implementation(project(":data"))
     implementation(project(":domain"))
-
-    /* Coil */
-    implementation("io.coil-kt:coil:2.7.0")
-
-    /* Retrofit */
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-
-    /* STOMP */
-    implementation("com.github.NaikSoftware:StompProtocolAndroid:1.6.6")
-    implementation("com.squareup.okhttp3:okhttp:3.8.0")
-    implementation("io.reactivex.rxjava2:rxjava:2.2.21")
-
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.activity:activity-ktx:1.8.2")
-    implementation("androidx.fragment:fragment-ktx:1.6.2")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
-    implementation("androidx.datastore:datastore-preferences:1.0.0")
-    implementation("androidx.paging:paging-runtime-ktx:3.3.0-alpha04")
-    implementation("com.google.dagger:hilt-android:2.44")
-    kapt("com.google.dagger:hilt-android-compiler:2.44")
 }
 
 kapt {
